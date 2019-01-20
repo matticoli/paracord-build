4ab704ba870db801c15c5e64defbc9dc3ec3638d
matticoli <mikel.matticoli@gmail.com>
2019-01-20 07:12:15 -0500

    build:

---
+++ precache-manifest.217511a29f69cf5a44b13dd50de329fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49c26f9bf3c8bebe68ca",
+    "url": "/static/js/main.49c26f9b.chunk.js"
+  },
+  {
+    "revision": "1c927a8f2507ad9a5781",
+    "url": "/static/js/1.1c927a8f.chunk.js"
+  },
+  {
+    "revision": "49c26f9bf3c8bebe68ca",
+    "url": "/static/css/main.558e1d18.chunk.css"
+  },
+  {
+    "revision": "1c927a8f2507ad9a5781",
+    "url": "/static/css/1.7903c1a4.chunk.css"
+  },
+  {
+    "revision": "70c33d55986db973365177827afdf424",
+    "url": "/index.html"
+  }
+];
